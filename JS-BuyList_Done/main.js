98f086ab9024bd5e9ec4377a418d6197b1e02791
oleksandra5741 <32238817+oleksandra5741@users.noreply.github.com>
2017-12-15 01:16:55 +0200

    Update main.js

--- JS-BuyList_Done/main.js
+++ JS-BuyList_Done/main.js
@@ -1,174 +1,123 @@
 $(function(){
-
-    var $list = $(".bl-list");
-    var ONE_ROW_TEMPLATE = $(".one-row-template").html();
-
-    var $list_right_left = $(".bl-left");
-    var $list_right_bought = $(".bl-final");
-    var ONE_LEFT_ITEM_TEMPLATE = $(".final-template").html();
-    var ONE_BOUGHT_ITEM_TEMPLATE = $(".left-template").html();
-
-    var $input = $(".addForm");
-
-    function addItem(title) {
-        var $node = $(ONE_ROW_TEMPLATE);
-        var $nodeLeft = $(ONE_LEFT_ITEM_TEMPLATE);
-        var $nodeFinal = $(ONE_BOUGHT_ITEM_TEMPLATE);
-
-        var quantity = 1;
-
-        var $quantity_label = $node.find(".bl-label");
-        $quantity_label.text(quantity);
-
-        var $quantity_label_right = $nodeLeft.find(".bl-number");
-        $quantity_label_right.text(quantity);
-
-        var $quantity_label_bought = $nodeFinal.find(".bl-number");
-        $quantity_label_bought.text(quantity);
-
-        $node.find(".bl-product").text(title);
-        $nodeLeft.find(".bl-text").text(title);
-        $nodeFinal.find(".bl-text").text(title);
-
-        $nodeFinal.hide();
-        $node.find(".bl-name").hide();
-
-        $node.find(".unbought").css("display","none");
-
-        $node.find(".bl-product").click(function() {
-            $node.find(".bl-product").hide();
-            $node.find(".bl-name").show();
-            $node.find(".bl-name").val(title);
-            $node.find(".bl-name").focus();
-        });
-
-        $node.find(".bl-name").focusout(function() {
-            title = $node.find(".bl-name").val();
-            $node.find(".bl-name").hide();
-            $node.find(".bl-product").show();
-
-            if (title.trim()){
-                $node.find(".bl-product").text(title);
-                $nodeLeft.find(".bl-text").text(title);
-                $nodeFinal.find(".bl-text").text(title);
-            }
-        });
-
-        $node.find(".bl-name").keydown(function(e) {
-            if (e.which === 13) {
-                title = $node.find(".bl-name").val();
-                $node.find(".bl-name").hide();
-                $node.find(".bl-product").show();
-
-                if (title.trim()){
-                    $node.find(".bl-product").text(title);
-                    $nodeLeft.find(".bl-text").text(title);
-                    $nodeFinal.find(".bl-text").text(title);
-                }
-            }
-        })
-
-        $node.find(".bl-minus").click(function() {
-            $node.find(".bl-label").fadeOut("normal", function(){
-                if (quantity > 1) {
-                    quantity--;
-                    $quantity_label.text(quantity);
-                    $quantity_label_right.text(quantity);
-                    $quantity_label_bought.text(quantity);
-
-                    if (quantity === 1){
-                        $node.find(".bl-minus").css("opacity","0.5");
-                        $node.find(".bl-minus").prop("disabled", true);
-                    }
-                }
-            });
-            $node.find(".bl-label").fadeIn();
-        });
-
-        $node.find(".bl-plus").click(function() {
-            $node.find(".bl-label").fadeOut("normal", function() {
-                quantity++;
-                $quantity_label.text(quantity);
-                $quantity_label_right.text(quantity);
-                $quantity_label_bought.text(quantity);
-                if (quantity > 1) {
-                    $node.find(".bl-minus").css("opacity", "1");
-                    $node.find(".bl-minus").prop("disabled", false);
-                }
-            });
-            $node.find(".bl-label").fadeIn();
-        });
-
-        $node.find(".bought").click(function(){
-            $node.fadeOut("normal", function() {
-                $node.find(".bought").css("display","none");
-                $node.find(".unbought").css("display","inline");
-                $node.find(".bl-delete").css("display","none");
-                $node.find(".bl-plus").css("visibility","hidden");
-                $node.find(".bl-minus").css("visibility","hidden");
-                $node.find(".bl-product").css("textDecoration","line-through");
-            });
-
-            $nodeFinal.show();
-            $nodeLeft.hide();
-
-            $node.fadeIn("normal");
-        });
-
-        $node.find(".unbought").click(function(){
-            $node.fadeOut("normal", function() {
-                $node.find(".bought").css("display","inline");
-                $node.find(".unbought").css("display","none");
-                $node.find(".bl-delete").css("display","inline");
-                $node.find(".bl-plus").css("visibility","visible");
-                $node.find(".bl-minus").css("visibility","visible");
-                $node.find(".bl-product").css("textDecoration","none");
-            });
-
-            $nodeFinal.hide();
-            $nodeLeft.show();
-
-            $node.fadeIn("normal");
-        });
-
-        $node.find(".bl-delete").click(function(){
-            $node.slideUp("normal", function () {
-                $node.remove();
-                $nodeLeft.remove();
-            });
-        });
-
-        $node.find(".bl-minus").css("opacity","0.5");
-        $node.find(".bl-minus").prop("disabled", true);
-
-        $list.append($node);
-        $list_right_left.append($nodeLeft);
-        $list_right_bought.append($nodeFinal);
-    }
-
-    $(".add").click(function() {
-        var name = $input.val();
-
-        if (name.trim()) {
-            addItem(name);
-            $input.val("");
-            $input.focus();
-        }
-    });
-
-    $input.keydown(function(e) {
-        if (e.which === 13) {
-            var name = $input.val();
-
-            if (name.trim()) {
-                addItem(name);
-                $input.val("");
-                $input.focus();
-            }
-        }
-    });
-
-    addItem("Помідори");
-    addItem("Печиво");
-    addItem("Сир");
+    var ONE_ROW_TEMPLATE = $(".OneRow").html();
+    var $LIST = $(".LIST");
+    var $LISTrbought = $(".FINAL");
+    var $LISTrleft = $(".LEFT");
+    var ONE_BOUGHT_ITEM_TEMPLATE = $(".LETEM").html();
+    var ONE_LEFT_ITEM_TEMPLATE = $(".FINTEM").html();
+    var $ENTERING = $(".FORMforADD");
+    function ADDPRODUCT(CAPTION) {
+        var $NODE = $(ONE_ROW_TEMPLATE);
+        var $NODEL = $(ONE_LEFT_ITEM_TEMPLATE);
+        var $NODEFIN = $(ONE_BOUGHT_ITEM_TEMPLATE);
+        var QUAN = 1;
+        var $QUAN_LABE = $NODE.find(".LABEL");
+        $QUAN_LABE.text(QUAN);
+        var $QUAN_LABE_right = $NODEL.find(".NUMBER");
+        $QUAN_LABE_right.text(QUAN);
+        var $QUAN_label_bought = $NODEFIN.find(".NUMBER");
+        $QUAN_label_bought.text(QUAN);
+        $NODEL.find(".TEXT").text(CAPTION);
+        $NODEFIN.find(".TEXT").text(CAPTION);
+        $NODE.find(".PRODUCT").text(CAPTION);
+        $NODEFIN.hide();
+        $NODE.find(".NOTCHECKED").css("display","none");
+        $NODE.find(".NAME").hide();
+        $NODE.find(".PRODUCT").click(function() {
+            $NODE.find(".PRODUCT").hide();
+            $NODE.find(".NAME").show();
+            $NODE.find(".NAME").val(CAPTION);
+            $NODE.find(".NAME").focus();});
+        $NODE.find(".NAME").focusout(function() {
+            CAPTION = $NODE.find(".NAME").val();
+            $NODE.find(".NAME").hide();
+            $NODE.find(".PRODUCT").show();
+            if (CAPTION.trim()){
+                $NODE.find(".PRODUCT").text(CAPTION);
+                $NODEL.find(".TEXT").text(CAPTION);
+                $NODEFIN.find(".TEXT").text(CAPTION);
+            }});
+        $NODE.find(".MINUS").click(function() {
+            $NODE.find(".LABEL").fadeOut("normal", function(){
+                if (QUAN > 1) {
+                    QUAN--;
+                    $QUAN_LABE.text(QUAN);
+                    $QUAN_LABE_right.text(QUAN);
+                    $QUAN_label_bought.text(QUAN);
+                    if (QUAN === 1){
+                        $NODE.find(".MINUS").css("opacity","0.5");
+                        $NODE.find(".MINUS").prop("disabled", true);
+                    }}});
+            $NODE.find(".NAME").keydown(function(e) {
+                if (e.which === 13) {
+                    CAPTION = $NODE.find(".NAME").val();
+                    $NODE.find(".NAME").hide();
+                    $NODE.find(".PRODUCT").show();
+                    if (CAPTION.trim()){
+                        $NODE.find(".PRODUCT").text(CAPTION);
+                        $NODEL.find(".TEXT").text(CAPTION);
+                        $NODEFIN.find(".TEXT").text(CAPTION);
+                    }}})
+            $NODE.find(".LABEL").fadeIn();});
+        $NODE.find(".CHECKED").click(function(){
+            $NODE.fadeOut("normal", function() {
+                $NODE.find(".CHECKED").css("display","none");
+                $NODE.find(".NOTCHECKED").css("display","inline");
+                $NODE.find(".DELETE").css("display","none");
+                $NODE.find(".PLUS").css("visibility","hidden");
+                $NODE.find(".MINUS").css("visibility","hidden");
+                $NODE.find(".PRODUCT").css("textDecoration","line-through");});
+            $NODEFIN.show();
+            $NODEL.hide();
+            $NODE.fadeIn("normal");});
+        $NODE.find(".NOTCHECKED").click(function(){
+            $NODE.fadeOut("normal", function() {
+                $NODE.find(".CHECKED").css("display","inline");
+                $NODE.find(".NOTCHECKED").css("display","none");
+                $NODE.find(".DELETE").css("display","inline");
+                $NODE.find(".PLUS").css("visibility","visible");
+                $NODE.find(".MINUS").css("visibility","visible");
+                $NODE.find(".PRODUCT").css("textDecoration","none");});
+            $NODEFIN.hide();
+            $NODEL.show();
+            $NODE.fadeIn("normal");});
+        $NODE.find(".DELETE").click(function(){
+            $NODE.slideUp("normal", function () {
+                $NODE.remove();
+                $NODEL.remove();
+            });});
+        $NODE.find(".PLUS").click(function() {
+            $NODE.find(".LABEL").fadeOut("normal",function(){
+                QUAN++;
+                $QUAN_LABE.text(QUAN);
+                $QUAN_LABE_right.text(QUAN);
+                $QUAN_label_bought.text(QUAN);
+                if (QUAN>1){
+                    $NODE.find(".MINUS").css("opacity", "1");
+                    $NODE.find(".MINUS").prop("disabled", false);
+                }});
+            $NODE.find(".LABEL").fadeIn();});
+        $NODE.find(".MINUS").css("opacity","0.5");
+        $NODE.find(".MINUS").prop("disabled", true);
+        $LIST.append($NODE);
+        $LISTrleft.append($NODEL);
+        $LISTrbought.append($NODEFIN);}
+    $(".ADDItem").click(function() {
+        var name = $ENTERING .val();
+        if (name.trim()){
+            ADDPRODUCT(name);
+            $ENTERING .val("");
+            $ENTERING .focus();
+        }});
+    $ENTERING .keydown(function(e){
+        if (e.which===13){
+            var name = $ENTERING .val();
+            if (name.trim()){
+                ADDPRODUCT(name);
+                $ENTERING .val("");
+                $ENTERING .focus();
+            }}});
+    ADDPRODUCT("Печиво");
+    ADDPRODUCT("Помідори");
+    ADDPRODUCT("Сир");
 });
